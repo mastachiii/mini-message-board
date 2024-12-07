2c5c1e5943261af78690498a2b9ad99df28907fb
mastachii <mastachii273@gmail.com>
2024-12-07 17:25:35 +0800

    feat: Created Message Database

---
+++ models/messages.js
@@ -0,0 +1,20 @@
+const messages = [
+    {
+        id: 1,
+        user: "BPitt95",
+        message: "Whats in the box???",
+        date: new Date(),
+    },
+    {
+        id: 2,
+        user: "throwaway1234",
+        message: "Express is great!",
+        date: new Date(),
+    },
+    {
+        id: 3,
+        user: "pyDev9483",
+        message: "bacon spam bacon spam bacon spam",
+        date: new Date(),
+    },
+];
